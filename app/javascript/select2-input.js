c49a70b9edae32df79fdac8b9d582a4173448d27
Myles Skinner <mskinner@infotech.com>
2022-01-01 01:28:36 -0500

    Article create, edit, and view. Stub index

--- app/javascript/select2-input.js
+++ app/javascript/select2-input.js
@@ -1,5 +1,5 @@
 import $ from 'jquery'
-import 'select2/dist/css/select2.css'
+// import 'select2/dist/css/select2.css'
 import 'select2'
 
 window.addEventListener('DOMContentLoaded', () => {
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,10 @@
 class Article < ApplicationRecord
 
+  include ActionView::Helpers::SanitizeHelper
+  
   belongs_to :classification
   belongs_to :issue
-  belongs_to :language
+  belongs_to :language, optional: true
   has_and_belongs_to_many :machines
   has_and_belongs_to_many :tags
 
@@ -12,5 +14,31 @@ class Article < ApplicationRecord
 
   def to_s
     title
+  end
+
+  # I realize these belong in a decorator... :)
+  def classification_id_display
+    classification.name
+  end
+
+  def issue_id_display
+    sanitize (issue.date_display +
+        (" &ndash; Vol. #{issue.volume}, No. #{issue.number}" unless issue.number.blank?))
+  end
+
+  def language_id_display
+    language.name
+  end
+
+  def machine_ids_display
+    machines.pluck(:name).join(', ')
+  end
+  
+  def magazine_id_display
+    issue.magazine.to_s
+  end
+
+  def tag_ids_display
+    tags.pluck(:name).join(', ')
   end
 end
