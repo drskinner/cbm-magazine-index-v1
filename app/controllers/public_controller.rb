735b0667dbec58c458e645aada463173e423dad9
Myles Skinner <mskinner@infotech.com>
2022-02-24 20:08:09 -0500

    PR nitpicks

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -21,21 +21,7 @@ class PublicController < ApplicationController
     @articles = @articles.page(params[:page])
   end
 
-  def modal_test
-    sort = params[:sort] || :id
-    direction = params[:direction] || :asc
-
-    @articles = Article.accessible_by(current_ability)
-                       .search(params.slice(:by_magazine_id, :by_year))
-    @full_count = @articles.count
-
-    # @articles = @articles.order(sort => direction)
-    #                  .page(params[:page])
-    @articles = @articles.page(params[:page])
-  end
-
   def article
-    puts 'HERE'
     @article = Article.find(params[:id])
     respond_to do |format|
       format.js {render layout: false}
