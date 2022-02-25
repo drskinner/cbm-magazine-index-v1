edc005f029e7c7ee5aa6dfd6c4934fd77f6510db
Myles Skinner <mskinner@infotech.com>
2022-02-24 19:57:27 -0500

    Modal display for single result

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -20,4 +20,25 @@ class PublicController < ApplicationController
     #                  .page(params[:page])
     @articles = @articles.page(params[:page])
   end
+
+  def modal_test
+    sort = params[:sort] || :id
+    direction = params[:direction] || :asc
+
+    @articles = Article.accessible_by(current_ability)
+                       .search(params.slice(:by_magazine_id, :by_year))
+    @full_count = @articles.count
+
+    # @articles = @articles.order(sort => direction)
+    #                  .page(params[:page])
+    @articles = @articles.page(params[:page])
+  end
+
+  def article
+    puts 'HERE'
+    @article = Article.find(params[:id])
+    respond_to do |format|
+      format.js {render layout: false}
+    end
+  end
 end
