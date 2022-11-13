8cfbdc7712d4802bc4e23af646ab1692b30be113
Myles Skinner <mskinner@infotech.com>
2022-11-13 00:20:26 -0500

    Add links to archive.org

--- app/helpers/issue_helper.rb
+++ app/helpers/issue_helper.rb
@@ -1,26 +1,37 @@
 module IssueHelper
+  def archive_base_url(issue)
+    # useless local variable for readability:
+    my_slug = issue.magazine.slug
+
+    "cbm_magazine_index-#{my_slug}/#{my_slug}"
+  end
+
   def archive_slug(issue)
     # useless local variables for readability:
     my_slug = issue.magazine.slug
     my_sequence = issue.special_slug || issue.sequence
 
-    "#{my_slug}/#{issue.year}/#{my_slug}-#{my_sequence}-#{issue.year}#{'%02i' % issue.month}"
+    "#{issue.year}/#{my_slug}-#{'%02i' % my_sequence}-#{issue.year}#{'%02i' % issue.month}"
+  end
+
+  def article_page(page, issue)
+    "page/n#{page + issue.page_offset}/mode/2-up"
   end
 
   def archive_download_link(issue)
-    "https://archive.org/download/cbm-magazine-index/#{archive_slug(issue)}.pdf"
+    "https://archive.org/download/#{archive_base_url(issue)}/#{archive_slug(issue)}.pdf"
   end
 
   def archive_jpeg_link(issue)
-    "https://archive.org/download/cbm-magazine-index/#{archive_slug(issue)}_jp2.zip"
+    "https://archive.org/download/#{archive_base_url(issue)}/#{archive_slug(issue)}_jp2.zip"
   end
 
   def archive_ocr_link(issue)
-    "https://archive.org/stream/cbm-magazine-index/#{archive_slug(issue)}_djvu.txt"
+    "https://archive.org/stream/#{archive_base_url(issue)}/#{archive_slug(issue)}_djvu.txt"
   end
 
-  def archive_view_link(issue)
-    "https://archive.org/details/cbm-magazine-index/#{archive_slug(issue)}/"
+  def archive_view_link(issue, page)
+    "https://archive.org/details/#{archive_base_url(issue)}/#{archive_slug(issue)}/#{article_page(page, issue)}?view=theater"
   end
 
   def cover_image_url(issue)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_184826) do
+ActiveRecord::Schema.define(version: 2022_11_13_050252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_09_15_184826) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "special_slug"
+    t.integer "archive_page_offset", default: 0, null: false
     t.index ["magazine_id"], name: "index_issues_on_magazine_id"
   end
 
